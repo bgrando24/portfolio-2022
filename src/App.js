620111d7c7b227a52098d221605e9200d801daff
Brandon <bgrando24@gmail.com>
2022-10-10 20:15:05 +1100

    Set up react app with tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container bg-gray-500'>
+      <p className='text-center text-white-100'>Welcome to tailwind!</p>
+    </div>    
+  );
+}
+
+export default App;
