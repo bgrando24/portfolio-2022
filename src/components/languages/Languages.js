e81de875e8d71a0b42c2a7652d84526f8afe5604
bgrando24 <bgrando24@gmail.com>
2022-10-16 10:29:46 +1100

    Styling updates

--- src/components/languages/Languages.js
+++ src/components/languages/Languages.js
@@ -2,18 +2,22 @@ import { Tooltip } from "@material-tailwind/react"
 
 const languages = [
     {name: "JavaScript", description: "Use of JavaScript to build dynamic and interactive websites following ES6 standards"},
-    {name: "HTML5", description: ""},
-    {name: "CSS", description: ""},
-    {name: "React.js", description: ""},
-    {name: "Node.js", description: ""},
-    {name: "Express.js", description: ""},
+    {name: "HTML5", description: "Clean, simple and effective use of HTML to structure frontend designs"},
+    {name: "CSS", description: "Use of raw CSS and tools such as Tailwind to create clean and visually appealing interfaces"},
+    {name: "React", description: "Use of functional components and React hooks to develop a clean and consistent frontend"},
+    {name: "Node", description: "Used in full-stack applications I have developed, primarily in running instances of Express.js for backends"},
+    {name: "Express", description: "Secure and functional sever instances with Express to create full-stack applications, including interfacing with databases"},
+    {name: "PostgreSQL", description: "Use of both raw SQL statements and integration with Express to effectively create, read, update and delete data"},
+    {name: "GitHub", description: "Use of Github for both version control and for cross-platform development between different devices"},
+    {name: "CLI", description: "Use of MacOS and Windows terminal for version control, running scripts and other development functions"},
+    {name: "C++", description: "While not part of my primary stack, I have used C++ to develop small CLI and graphical applications"}
 ]
 
 export function Languages() {
     return (
         <div className="container mx-auto h-auto bg-white -mt-20 py-10 mb-20 rounded-3xl shadow-lg">
 
-            <h1 className="text-center text-3xl text-bold mb-8">Languages</h1>
+            <h1 className="text-center text-3xl text-bold mb-8">The tech I've learnt</h1>
 
             <div className="flex flex-wrap justify-center">
                 {
@@ -26,7 +30,7 @@ export function Languages() {
                                 unmount: { scale: 0, y: 25 },
                             }}
                             >
-                                <div className="flex justify-center items-center bg-gray-200 shadow w-72 min-w-[25%] h-20 m-1 hover:scale-110 transition duration-200">
+                                <div className="flex justify-center items-center bg-gray-200 font-bold shadow w-72 min-w-[25%] h-20 m-1 hover:scale-110 transition duration-200">
                                     <div key={i}>{language.name}</div>   
                                 </div>
                             </Tooltip>
