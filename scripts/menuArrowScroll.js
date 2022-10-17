90871a6d5ef08b8ed3c5cec013fe16d65d1e51b0
bgrando24 <bgrando24@gmail.com>
2022-10-17 15:48:12 +1100

    Updates

---
+++ scripts/menuArrowScroll.js
@@ -0,0 +1,16 @@
+// makes the menu toggle component visible when user scrolls
+const checkScroll = () => {
+    // console.log(window.scrollY);
+    // console.log(window.innerHeight); 
+    if(window.scrollY >= window.innerHeight/4) {
+        // console.log("Scrolled past trigger");
+        document.getElementById('menuArrowDiv').classList.add('show-menuArrow');
+    }
+    else {
+        document.getElementById('menuArrowDiv').classList.remove('show-menuArrow');
+    }
+}
+
+// document.getElementById('menuArrowDiv').style.visibility = 'hidden';
+
+window.addEventListener("scroll", checkScroll);
