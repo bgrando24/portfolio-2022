1ee13a56fd122a2e4f86f781ce52272958bc4e38
Brandon <bgrando24@gmail.com>
2022-10-27 21:07:23 +1100

    Updates

--- src/pages/home/components/projects/Projects.js
+++ src/pages/home/components/projects/Projects.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 const projects = [
     {name: "Todo App",              path:"https://github.com/bgrando24/todo-app",           image: todo_app},
-    {name: "This Website!",         path:"/about",                            image: portfolio},
+    {name: "This Website!",         path:"https://github.com/bgrando24/portfolio-2022",     image: portfolio},
     {name: "Reddit Client",         path:"https://bgrando24.github.io/reddit-client",       image: seenit},
 ]
 
@@ -47,7 +47,7 @@ export function Projects() {
 // Component for simplifyting the project button
 const ProjectButton = ({ name, path }) => {
     return (
-        <a href={path}>
+        <a href={path} target="_blank">
             <div className="flex flex-col h-full justify-center items-center z-1">
                 <h1 className=" font-bold text-white relative mb-3">{name}</h1>
                 <button id={"project-button-"+name} className="text-center text-white bg-blue-600 rounded-lg w-32 h-10 cursor-pointer hover:bg-black duration-200">View Project</button>
